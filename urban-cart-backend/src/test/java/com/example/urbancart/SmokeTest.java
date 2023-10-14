2b9b5b2ed5734119d5e6ae913500d178d5daf659
nimisha <nimisha.mec@gmail.com>
2023-10-14 18:57:42 -0400

    initial commit

---
+++ urban-cart-backend/src/test/java/com/example/urbancart/SmokeTest.java
@@ -0,0 +1,13 @@
+package com.example.urbancart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
