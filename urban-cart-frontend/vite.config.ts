189898fcd7ffa2f8788d202e59100945438f9c69
shrestu <umang.shrestha@autodesk.com>
2023-10-15 19:18:16 -0400

    adding unittest and testcoverage for frontend

--- urban-cart-frontend/vite.config.ts
+++ urban-cart-frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc'
+
+const TestCoverage = 50;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      lines: TestCoverage,
+      statements: TestCoverage,
+      functions: TestCoverage,
+    }
+  },
 })
